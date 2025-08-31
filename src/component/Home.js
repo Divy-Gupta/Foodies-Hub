9536598c698eda864793d0234d72de589cc640c8
Divy Gupta <154852793+Divy-Gupta@users.noreply.github.com>
2025-08-31 23:24:42 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,11 +4,20 @@ const Home = () => {
   return (
     <>
     <section className='home' id='home'>
-        <div className='content'>
-            <h1><span>what to cook ?!</span></h1>
-            <p> No need to wonder — just enter or upload ingredients and get instant recipes.</p>
-        </div>
-    </section>
+  <video autoPlay muted loop className="bg-video">
+    <source src="/128126-740854563_tiny.mp4" type="video/mp4" />
+  </video>
+  <div className='content'>
+    <h1><span>What to cook ?!</span></h1>
+    <p>No need to wonder — just enter or upload ingredients and get instant recipes.</p>
+    <div className="home-buttons">
+      <button onClick={() => document.getElementById("search-btn").click()}>Search Recipes</button>
+      <button onClick={() => document.getElementById("image-upload").click()}>Upload Image</button>
+      <button onClick={() => document.getElementById("liked").click()}>View Favorites</button>
+    </div>
+  </div>
+</section>
+
     </>
   )
 }
